793f9328f170531591dfa29e526ee3e4ce75ccbe
Joseph DiLauro <dilauro74@gmail.com>
2019-10-31 17:34:58 -0400

    Added Test Class

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+
+    public double add(double[] nums) {
+        double sum = 0;
+        for (int i=0; i < nums.length; i++){
+            sum = sum + nums[i];
+        }
+        return sum;
+    }
+
+    public double subtract(double a, double b) {
+        double total = a-b;
+        return total;
+    }
+
+    public double multiply(double[] nums) {
+        double product = 1;
+        for (int i=0; i < nums.length; i++){
+            product = product * nums[i];
+        }
+        return product;
+    }
+
+    public double divide(double dividend, double divisor){
+        double quotient = dividend/divisor;
+        return quotient;
+    }
+
+}
---
+++ src/test/java/TestCalculator.java
@@ -0,0 +1,56 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestCalculator {
+
+    @Test
+    public void testAdd(){
+        double[] nums = new double[4];
+        nums[0] = 1.02;
+        nums[1] = 3.05;
+        nums[2] = 4.98;
+        nums[3] = 0.08;
+
+        Calculator c = new Calculator();
+
+        //assertions
+        assertEquals(9.13, c.add(nums), .01);   //expected, actual, epsilon (acceptable variance)
+    }
+
+    @Test
+    public void testMultiply(){
+        double[] nums = new double[4];
+        nums[0] = 1.02;
+        nums[1] = 3.05;
+        nums[2] = 4.98;
+        nums[3] = 0.08;
+
+        Calculator c = new Calculator();
+
+        //assertions
+        assertEquals(1.24, c.multiply(nums), .01);   //expected, actual, epsilon (acceptable variance)
+    }
+
+    @Test
+    public void testDivide(){
+        double dividend = 8.19;
+        double divisor = 4.21;
+
+        Calculator c = new Calculator();
+
+        //assertions
+        assertEquals(1.94, c.divide(dividend, divisor), .01);   //expected, actual, epsilon (acceptable variance)
+    }
+
+    @Test
+    public void testSubtract(){
+        double a = 8.19;
+        double b = 4.21;
+
+        Calculator c = new Calculator();
+
+        //assertions
+        assertEquals(3.98, c.subtract(a, b), .01);   //expected, actual, epsilon (acceptable variance)
+    }
+
+}
